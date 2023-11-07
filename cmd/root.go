2ee21e33dbcb6538b7d7c4c5f4fb29a277dfae56
Isaque Veras <isaqueveras@alu.ufc.br>
2023-11-07 20:59:59 -0300

    feat: add cobra cli

---
+++ cmd/root.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "zoity",
+		Short: "Zoity is an orchestrator for configuring and running services locally.",
+	}
+
+	versionCommand = &cobra.Command{
+		Use: "version",
+		Run: func(_ *cobra.Command, _ []string) {
+			fmt.Println("Zoity version v0.0.0")
+		},
+	}
+
+	addServiceCommand = &cobra.Command{
+		Use:   "add",
+		Short: "Configure a service",
+		Run:   func(cmd *cobra.Command, args []string) {},
+	}
+)
+
+func Execute() {
+	rootCmd.AddCommand(versionCommand, addServiceCommand)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/isaqueveras/zoity/cmd"
+
+func main() {
+	cmd.Execute()
+}
