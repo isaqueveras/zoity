06e39f650dc85363a21171c89f7d127bba26fee5
Isaque Veras <isaqueveras@alu.ufc.br>
2025-02-18 17:40:53 -0300

    feat: add gocui

--- go.mod
+++ go.mod
@@ -1,10 +1,16 @@
 module github.com/isaqueveras/zoity
 
-go 1.21.3
+go 1.23
 
-require github.com/spf13/cobra v1.8.0
+require (
+	github.com/jesseduffield/gocui v0.3.0
+	github.com/spf13/cobra v1.8.0
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jroimartin/gocui v0.5.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "github.com/isaqueveras/zoity/cmd"
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/jroimartin/gocui"
+)
 
 func main() {
-	cmd.Execute()
+	g, err := gocui.NewGui(gocui.Output256)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer g.Close()
+
+	// g.CurrentView().Highlight = true
+	g.Cursor = true
+	g.SelFgColor = gocui.ColorRed
+
+	g.SetManagerFunc(func(g *gocui.Gui) error {
+		maxX, maxY := g.Size()
+
+		if v, err := g.SetView("projects", 1, 1, 2, 2); err != nil {
+			v.Title = "Projects"
+
+			fmt.Fprintln(v, "Projects")
+		}
+
+		if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+			if _, err := g.SetCurrentView("hello"); err != nil {
+				return err
+			}
+
+			fmt.Fprintln(v, "Hello world!")
+		}
+
+		return nil
+	})
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, func(g *gocui.Gui, v *gocui.View) error {
+		return gocui.ErrQuit
+	}); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && !errors.Is(err, gocui.ErrQuit) {
+		log.Panic(err.Error())
+	}
 }
